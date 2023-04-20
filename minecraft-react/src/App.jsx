095395834916f6100f362a757b708684bb87b2b3
yisus82 <djalma8@gmail.com>
2023-04-20 13:55:05 +0200

    Sky & Ambient Light

--- minecraft-react/src/App.jsx
+++ minecraft-react/src/App.jsx
@@ -1,7 +1,13 @@
-import './App.css';
+import { Sky } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 
 const App = () => {
-  return <p>Hola!!!</p>;
+  return (
+    <Canvas>
+      <Sky sunPosition={[100, 100, 20]} />
+      <ambientLight intensity={0.5} />
+    </Canvas>
+  );
 };
 
 export default App;
